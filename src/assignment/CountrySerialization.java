902d4e5e9d8b413b29118073cc6e6844cc9607d6
MusanjeLawrence <musanjelawrence6@gmail.com>
2023-03-30 16:16:58 +0300

    first commit containing the client server and serializable classes of the project as well as some working buttons designed and programmed in java

---
+++ src/assignment/CountrySerialization.java
@@ -0,0 +1,11 @@
+
+package assignment;
+import java.io.Serializable;
+
+public class CountrySerialization implements Serializable {
+    public CountrySerialization(){
+        String country_name = "";
+     String country_code="";
+    String login_username="";
+    }
+}
